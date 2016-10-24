279745ae6b277cbc6d696c2a4c4ea397dc7990e3
Gerard de Jong <gerard@dejong.za.net>
2016-10-24 10:40:42 +0200

    Moving code over to it's own repository for further development

---
+++ Schema Validator/Schema Validator.h
@@ -0,0 +1,19 @@
+//
+//  Schema Validator.h
+//  Schema Validator
+//
+//  Created by Gerard de Jong on 2016/10/11.
+//  Copyright © 2016 Gerard de Jong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Schema Validator.
+FOUNDATION_EXPORT double Schema_ValidatorVersionNumber;
+
+//! Project version string for Schema Validator.
+FOUNDATION_EXPORT const unsigned char Schema_ValidatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Schema_Validator/PublicHeader.h>
+
+
